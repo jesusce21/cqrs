3a95264374b6623dadcb05ade4cb3167e75faba4
Jesus Cabezas <jesusctic@gmail.com>
2020-06-13 09:26:18 +0200

    Change minimmun version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="cqrs",
-    version="0.0.1",
+    version="1.0.0",
     author="Jesusce",
     author_email="jesusctic@gmail.com",
     description="A small example package",
